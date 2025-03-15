255d701d56713fcb4014feafafae66c4f607abcb
dines14-coder <150772284+dines14-coder@users.noreply.github.com>
2025-03-15 13:05:25 +0530

    Update environment.prod.ts

--- SprintTrack-UI/src/environments/environment.prod.ts
+++ SprintTrack-UI/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://localhost:55046/'
+  apiUrl: 'http://localhost:5006/api/'
 };
