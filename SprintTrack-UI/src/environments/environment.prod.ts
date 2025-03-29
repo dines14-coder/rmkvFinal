14dde6407a8403c8ba8bd0180e472886dae9e34e
dines14-coder <150772284+dines14-coder@users.noreply.github.com>
2025-03-29 16:19:24 +0530

    Update environment.prod.ts

--- SprintTrack-UI/src/environments/environment.prod.ts
+++ SprintTrack-UI/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://track-service:8080/api/'
+  apiUrl: 'http://track-service:80/api/'
 };
